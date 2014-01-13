f41295ecc8ec4a927b310a56ef03450dc9a375e3
David J Eddy <me@davidjeddy.com>
2014-01-13 12:40:49 -0500

    Added the twtbs theme 'yeti' to the application.

--- views/templates/top.php
+++ views/templates/top.php
@@ -4,4 +4,5 @@
 
     <link rel="stylesheet" type="text/css" href="<?= SITEHOME; ?>global_assets/css/reset.css">
     <link rel="stylesheet" type="text/css" href="<?= SITEHOME; ?>vendor/twitter/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="<?= SITEHOME; ?>global_assets/twtbs_themes/yeti.bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="<?= SITEHOME; ?>global_assets/css/windsnet.css">
