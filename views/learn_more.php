b89d5bf9a428fc8de9c32928d3156da1fa92ef9e
David J Eddy <me@davidjeddy.com>
2014-01-13 12:41:12 -0500

    Added 'learn_more' and 'sign_up' view files.

---
+++ views/learn_more.php
@@ -0,0 +1,22 @@
+<?php require_once "../config.php"; ?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <?php require_once SITEROOT."/templates/top.php"; ?>
+</head>
+
+<body>
+    <noscript>
+        <h3>This service requires javascript to be enabled.</h3>
+        <h4>Please turn it on in your browser and refresh the page for proper operation.</h4>
+    </noscript>
+
+    <?php require_once SITEROOT."/templates/top_menu.php"; ?>
+
+    
+
+    <?php require_once SITEROOT."/templates/bottom.php"; ?>
+
+</body>
+</html>
