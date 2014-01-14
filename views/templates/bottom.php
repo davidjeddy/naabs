673d8183fcb6c011ac7caf1ff88b5f14a811d99c
David J Eddy <me@davidjeddy.com>
2014-01-14 17:26:22 -0500

    See pre. commit msg.

--- views/templates/bottom.php
+++ views/templates/bottom.php
@@ -7,6 +7,7 @@
 
 <!--// Manually added libs -->
 <script language="javascript" src="<?= SITEHOME; ?>global_assets/js/jquery_validate/dist/jquery.validate.min.js" ></script>
+<script language="javascript" src="<?= SITEHOME; ?>global_assets/js/jquery.unserialize.js" ></script>
 
 <!--// Application JS logic -->
 <script language="javascript" src="<?= SITEHOME; ?>global_assets/js/windsnet.js"></script>
