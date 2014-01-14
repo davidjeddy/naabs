57a11dc475f0ece9cde5a2bbef3f0adc9eec3bbf
David J Eddy <me@davidjeddy.com>
2014-01-14 15:23:48 -0500

    Added the jquery-cookie library to make cookie operations easier.

--- views/templates/bottom.php
+++ views/templates/bottom.php
@@ -3,6 +3,7 @@
 <script language="javascript" src="<?= SITEHOME; ?>vendor/twitter/bootstrap/dist/js/bootstrap.min.js"></script>
 <script language="javascript" src="<?= SITEHOME; ?>vendor/balupton/jquery-scrollto/src/documents/lib/jquery-scrollto.js"></script>
 <script language="javascript" src="<?= SITEHOME; ?>vendor/makeusabrew/bootbox/bootbox.js"></script>
+<script language="javascript" src="<?= SITEHOME; ?>vendor/carhartl/jquery-cookie/jquery.cookie.js"></script>
 
 <!--// Manually added libs -->
 <script language="javascript" src="<?= SITEHOME; ?>global_assets/js/jquery_validate/dist/jquery.validate.min.js" ></script>
