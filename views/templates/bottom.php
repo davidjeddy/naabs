4de005dfd509f6e244c6c8cbc39482f7c5b76f7f
David J Eddy <me@davidjeddy.com>
2014-01-14 12:30:04 -0500

    Added the jquery_validate include to ./template/bottom.php so it is available on all pages.

--- views/templates/bottom.php
+++ views/templates/bottom.php
@@ -1,5 +1,11 @@
+<!--// jQ and other jQ libas added via composer -->
 <script language="javascript" src="<?= SITEHOME; ?>vendor/components/jquery/jquery.min.js"></script>
 <script language="javascript" src="<?= SITEHOME; ?>vendor/twitter/bootstrap/dist/js/bootstrap.min.js"></script>
 <script language="javascript" src="<?= SITEHOME; ?>vendor/balupton/jquery-scrollto/src/documents/lib/jquery-scrollto.js"></script>
 <script language="javascript" src="<?= SITEHOME; ?>vendor/makeusabrew/bootbox/bootbox.js"></script>
+
+<!--// Manually added libs -->
+<script language="javascript" src="<?= SITEHOME; ?>global_assets/js/jquery_validate/dist/jquery.validate.min.js" ></script>
+
+<!--// Application JS logic -->
 <script language="javascript" src="<?= SITEHOME; ?>global_assets/js/windsnet.js"></script>
