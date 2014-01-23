1c6201ea3c8e035517e10a11d5b59b7889176f4c
David J Eddy <me@davidjeddy.com>
2014-01-23 01:46:29 -0500

    Update the view in prep to display logged transactions.

--- views/my_history.php
+++ views/my_history.php
@@ -1,4 +1,11 @@
-<?php require_once "../config.php"; ?>
+<?php
+require_once __DIR__."/../config.php";
+require_once __DIR__."/templates/auth_check.php";
+require_once __DIR__."/../controllers/history_class.php";
+
+$historyClass = new historyClass;
+$h_data = $historyClass->readHistory($_COOKIE['USER']);
+?>
 <!DOCTYPE html>
 <html>
 
@@ -19,6 +26,11 @@
 
     <div class="well well-lg">
         <p>`My Access History` content here.</p>
+        <?php
+            foreach ($h_data as $transaction) {
+                print_r($transaction);
+            }
+        ?>
     </div>
 
     <?php require_once SITEROOT."/templates/bottom.php"; ?>
