6ec759e3d58328f32f60c023ee7bf2cdec4b8b48
David J Eddy <me@davidjeddy.com>
2014-02-08 16:57:10 -0500

    Moving 'no script' to a template

---
+++ views/templates/no_script.php
@@ -0,0 +1,7 @@
+<?php
+#No script HTML
+?>
+<noscript>
+    <h3>This service requires javascript to be enabled.</h3>
+    <h4>Please turn it on in your browser and refresh the page for proper operation.</h4>
+</noscript>
