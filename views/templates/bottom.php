6f86cd7ec52d62c4f9401523baef11fbf8d85eba
David J Eddy <me@davidjeddy.com>
2014-02-11 17:40:16 -0500

    Moved ajax loader to bottom.php so all pages have access to it. In theory it should work automatically site wide.

--- views/templates/bottom.php
+++ views/templates/bottom.php
@@ -18,3 +18,52 @@ if ($_SERVER["SERVER_ADDR"] == DEVIP) {
 	include_once( __DIR__.'/view_debug.php');
 }
 ?>
+
+<?php
+/**
+ * Basic full screen overlay during ajax processing
+ *
+ *@source http://stackoverflow.com/questions/1964839/jquery-please-wait-loading-animation
+ */
+ ?>
+ <style>
+/* Start by setting display:none to make this hidden.
+Then we position it in relation to the viewport window
+with position:fixed. Width, height, top and left speak
+speak for themselves. Background we set to 80% white with
+our animation centered, and no-repeating */
+.modal {
+    display:    none;
+    position:   fixed;
+    z-index:    1000;
+    top:        0;
+    left:       0;
+    height:     100%;
+    width:      100%;
+    background: rgba( 255, 255, 255, .66 ) 
+                url('<?= SITEROOT; ?>/../global_assets/images/ajax_processing.gif') 
+                50% 50% 
+                no-repeat;
+}
+
+/* When the body has the loading class, we turn
+   the scrollbar off with overflow:hidden */
+body.loading {
+    overflow: hidden;   
+}
+
+/* Anytime the body has the loading class, our
+   modal element will be visible */
+body.loading .modal {
+    display: block;
+}
+</style>
+    <div class="modal"><!-- Place at bottom of page --></div>
+<script>
+$body = $("body");
+
+$(document).on({
+    ajaxStart: function() { $body.addClass("loading");    },
+    ajaxStop: function() { $body.removeClass("loading"); }    
+});
+</script>
