3283c142aeb9239b23a4626e0221c46f27cd9d08
David J Eddy <me@davidjeddy.com>
2014-02-11 02:43:58 -0500

    First iteration of install file. It's basic, but it works.

---
+++ install/install.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Install script for `naabs`
+ */
+
+require_once( "../config.php" );
+
+echo "Attempting to create application log files...\n";
+exec( "mkdir -p ".SITEDIR." 2>&1");
+exec( "touch ".SITEDIR.SITELOG );
+exec( "chmod -R 0644 ".SITEDIR );
+exec( "chown -R www-data:www-data ".SITEDIR );
+
+// Add to log rotation
+//TODO add log rotation as part of the install script
+
+// Install dependances
+//install composer
+echo "Attempting `composer` install...\n";
+exec( "apt-get update && apt-get install composer" );
+
+// Install php5 cURL
+echo "Attempting `PHP5 cURL` install...\n";
+exec( "apt-get update && apt-get install php5-curl" );
+
+
+//run composer at the application root
+echo "Attempting to install application dependancies. This could take several minutes...\n";
+chdir("../");
+exec("composer self-update");
+exec("composer install");
+chdir("./install/");
+
+echo "Done. Make sure you install the database table(s).\n";
